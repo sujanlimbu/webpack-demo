4606bcb18cbf505e3589bc60e571d044eebf05a6
Sujan Khapung <sujanlimbu8@gmail.com>
2024-06-22 11:19:40 +1000

    webpack basic setup and asset management tutorial completed

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import _ from 'lodash';
+import myName from './myname';
+import './style.css';
+import Icon from './chart.png';
+import Data from './samplexml.xml';
+import Notes from './samplecsv.csv';
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
+
+console.log(toml.title); // output `TOML Example`
+console.log(toml.owner.name); // output `Tom Preston-Werner`
+
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
+
+function component() {
+    const element = document.createElement('div');
+
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    const name = myName('sujan');
+    element.append(name);
+    // element.textContent = myName('sujan');
+
+    element.classList.add('hello');
+
+    // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
+
+    console.log(Data);
+    console.log(Notes);
+
+    return element;
+}
+
+document.body.appendChild(component());
